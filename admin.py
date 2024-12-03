d701c4b22dc3d827958f365ca2b876d17a22a1c5
CJ <166865964+CJ-S7Z777Z@users.noreply.github.com>
2024-12-03 20:52:25 +0300

    Update admin.py

--- admin.py
+++ admin.py
@@ -1,10 +1,10 @@
-from config import ASSISTANT_MODEL, get_config, proxy_client, save_config
+from config import ASSISTANT_MODEL, get_config, client, save_config
 
 
 def get_vector_store_id():
     config = get_config()
     if "vector_store_id" not in config or not config["vector_store_id"]:
-        new_store = proxy_client.beta.vector_stores.create()
+        new_store = client.beta.vector_stores.create()
         config["vector_store_id"] = new_store.id
         save_config(config)
 
@@ -14,7 +14,7 @@ def get_vector_store_id():
 def create_assistant(name, instructions):
     assistant_id = get_assistant_id()
     if not assistant_id:
-        new_assistant = proxy_client.beta.assistants.create(
+        new_assistant = client.beta.assistants.create(
             model=ASSISTANT_MODEL,
             instructions=instructions,
             name=name,
@@ -31,7 +31,7 @@ def create_assistant(name, instructions):
         config["assistant_id"] = new_assistant.id
         save_config(config)
     else:
-        proxy_client.beta.assistants.update(
+        client.beta.assistants.update(
             assistant_id=assistant_id, instructions=instructions
         )
 
@@ -42,18 +42,18 @@ def get_assistant_id():
 
 
 def add_knowledge(filename, file):
-    file_object = proxy_client.files.create(file=(filename, file), purpose="assistants")
+    file_object = client.files.create(file=(filename, file), purpose="assistants")
     store_id = get_vector_store_id()
-    proxy_client.beta.vector_stores.files.create(
+    client.beta.vector_stores.files.create(
         vector_store_id=store_id, file_id=file_object.id
     )
 
 
 def reset_knowledge():
     store_id = get_vector_store_id()
-    files = proxy_client.beta.vector_stores.files.list(vector_store_id=store_id)
+    files = client.beta.vector_stores.files.list(vector_store_id=store_id)
     for file in files:
-        proxy_client.beta.vector_stores.files.delete(
+        client.beta.vector_stores.files.delete(
             vector_store_id=store_id, file_id=file.id
         )
-        proxy_client.files.delete(file.id)
+        client.files.delete(file.id)
